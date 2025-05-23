b4ecc3e9c6bc3ece973229a16de2587fdc0c363c
SantiBilli <santifbill@gmail.com>
2025-05-23 14:42:00 -0300

    feat: update API URL, enhance profile component, and add dropdown for obra social selection

--- components/ConfirmButton.tsx
+++ components/ConfirmButton.tsx
@@ -12,7 +12,7 @@ const ConfirmButton = ({ title, onPress, otherStyles, disabled }: props) => {
   return (
     <TouchableOpacity
       onPress={disabled ? undefined : onPress}
-      className={`w-full items-center rounded-[10px] border-[1px] border-secondary bg-[#DDF2FD] px-[55px] py-3 ${otherStyles} ${disabled ? 'opacity-50' : ''}`}>
+      className={`w-full items-center rounded-[10px] border-[1px] border-secondary bg-[#DDF2FD] px-[50px] py-3 ${otherStyles} ${disabled ? 'opacity-50' : ''}`}>
       <Text className="text-[14px] text-primary">{title}</Text>
     </TouchableOpacity>
   );
--- components/ModalObraSocial.tsx
+++ components/ModalObraSocial.tsx
@@ -1,12 +1,14 @@
-import { View, Text, Modal, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
-import { useState } from 'react'
-import Icon2 from 'react-native-vector-icons/Fontisto'
+import { View, Text, Modal, TouchableOpacity, TextInput } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import Icon2 from 'react-native-vector-icons/Fontisto';
 import RNPickerSelect from 'react-native-picker-select';
 import ConfirmButton from './ConfirmButton';
+import { Dropdown } from 'react-native-element-dropdown';
 
 interface props {
   modalOpen: boolean;
+  setModalOpen: (modalOpen: boolean) => void;
   obraSocial: string;
   plan: string;
   nroAfiliado: string;
@@ -16,46 +18,96 @@ interface props {
 }
 
 const obrasSociales = [
-    { label: 'OSDE', value: 'OSDE' },
-    { label: 'Swiss Medical', value: 'Swiss Medical' },
-    { label: 'Galeno', value: 'Galeno' },
-  ];
+  { label: 'OSDE', value: 'OSDE' },
+  { label: 'Swiss Medical', value: 'Swiss Medical' },
+  { label: 'Galeno', value: 'Galeno' },
+];
 
-  const planes = [
-    { label: 'Plan 210', value: 'Plan 210' },
-    { label: 'Plan 310', value: 'Plan 310' },
-    { label: 'Plan Premium', value: 'Plan Premium' },
-  ];
-const ModalObraSocial = ({modalOpen, obraSocial, plan, nroAfiliado, setObraSocial, setPlan, setNroAfiliado}: props) => {
-    
+const planes = [
+  { label: 'Plan 210', value: 'Plan 210' },
+  { label: 'Plan 310', value: 'Plan 310' },
+  { label: 'Plan Premium', value: 'Plan Premium' },
+];
+const ModalObraSocial = ({
+  modalOpen,
+  setModalOpen,
+  obraSocial,
+  plan,
+  nroAfiliado,
+  setObraSocial,
+  setPlan,
+  setNroAfiliado,
+}: props) => {
   return (
     <View>
       <Modal visible={modalOpen} transparent={true}>
-              <View className='flex-1 items-center justify-center bg-black/50'>
-                <View className='bg-background w-[85%] rounded-[10px] border-[1px] border-secondary'>
-                  <TouchableOpacity>
-                    <Icon2 name='close-a' size={19} color='#3ab4e5'className='self-end p-4'/>
-                  </TouchableOpacity>
-                  <View className='px-4'>
-                    <Text className='text-[15px] text-primary'>Obra Social</Text>
-                    <RNPickerSelect onValueChange={setObraSocial} items={obrasSociales} placeholder={{label: obraSocial, value: obraSocial}} value={obraSocial}/>
-                  </View>
-                  <View className='px-4'>
-                    <Text className='text-[15px] text-primary'>Plan</Text>
-                    <RNPickerSelect onValueChange={setPlan} items={planes} placeholder={{label: plan, value: plan}} value={plan}/>
-                  </View>
-                  <View className='px-4 mb-4'>
-                    <Text className='text-[15px] text-primary'>Nro Afiliado</Text>
-                    <TextInput className='flex text-[14px] h-[35px] border-b-[1px] border-secondary' value={nroAfiliado} onChangeText={(text) => setNroAfiliado(text)}/>
-                  </View>
-                  <View className='px-[85px] my-5'>
-                    <ConfirmButton title='Confirmar' onPress={() => {}} />
-                  </View>
-                </View>
+        <View className="flex-1 items-center justify-center bg-black/50">
+          <View className="w-[85%] rounded-[10px] border-[1px] border-secondary bg-background">
+            <TouchableOpacity onPress={() => setModalOpen(false)} className="self-end ">
+              <Icon2 name="close-a" size={19} color="#3ab4e5" className="p-4" />
+            </TouchableOpacity>
+            <View className="flex gap-5">
+              <View className="px-8">
+                <Text className="mb-1 text-[15px] text-primary">Obra Social</Text>
+                <Dropdown
+                  data={obrasSociales}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Seleccioná obra social"
+                  value={obraSocial}
+                  onChange={(item) => setObraSocial(item.value)}
+                  style={{
+                    height: 40,
+                    borderBottomWidth: 1,
+                    borderColor: '#45CADE',
+                    paddingHorizontal: 10,
+                  }}
+                  placeholderStyle={{ color: '#B2B2B2', fontSize: 14 }}
+                  selectedTextStyle={{ color: '#000', fontSize: 14 }}
+                  containerStyle={{ borderRadius: 6 }}
+                />
               </View>
-            </Modal>
+              <View className="px-8">
+                <Text className="text-[15px] text-primary">Plan</Text>
+                <Dropdown
+                  data={planes}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Seleccioná plan de obra social"
+                  value={plan}
+                  onChange={(item) => setPlan(item.value)}
+                  style={{
+                    height: 40,
+                    borderBottomWidth: 1,
+                    borderColor: '#45CADE',
+                    paddingHorizontal: 10,
+                  }}
+                  placeholderStyle={{ color: '#B2B2B2', fontSize: 14 }}
+                  selectedTextStyle={{ color: '#000', fontSize: 14 }}
+                  containerStyle={{ borderRadius: 6 }}
+                />
+              </View>
+              <View className="mb-4 px-8">
+                <Text className="text-[15px] text-primary">Nro Afiliado</Text>
+                <TextInput
+                  className="flex h-[40px] border-b-[1px] border-secondary px-[10px] text-[14px]"
+                  value={nroAfiliado}
+                  onChangeText={(text) => setNroAfiliado(text)}
+                />
+              </View>
+              <View className="my-5 px-[70px]">
+                <ConfirmButton
+                  title="Confirmar"
+                  onPress={() => {}}
+                  disabled={obraSocial == '' || plan == '' || nroAfiliado == ''}
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
-  )
-}
+  );
+};
 
-export default ModalObraSocial
+export default ModalObraSocial;
