86b2731de0957b84d2423a0df0c7f96b5246178a
nicoestepa9 <nicoo.estepaa@gmail.com>
2025-05-28 23:08:56 -0300

    feat: implement token validation with error handling and user feedback

--- app/(recovery)/insertToken.tsx
+++ app/(recovery)/insertToken.tsx
@@ -3,17 +3,48 @@ import ConfirmButton from '../../components/ConfirmButton';
 import FormTextInput from '../../components/FormTextInput';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'expo-router';
+import { useFetch } from '../../hooks/Fetch';
 
 const InsertToken = () => {
   const [token, setToken] = useState('');
+  const [invalidToken, setInvalidToken] = useState(false);
   const router = useRouter();
-  
-  const handlePress = () => {
-    router.push({
-      pathname: '/(recovery)/changePassword',
-      params: { token }
-    })
-  }
+  const [trigger, setTrigger] = useState(false);
+
+  const { data, error, isLoading } = useFetch({
+    endpoint: `/validate-token/${token}`,
+    method: 'POST',
+    trigger: trigger,
+    sendToken: false,
+  });
+
+  useEffect(() => {
+    if (trigger) return setTrigger(false);
+  }, [trigger]);
+
+  useEffect(() => {
+    if (isLoading || !data) return;
+    if (data.status === 200 && data.msg === 'Token valido') {
+      router.push({
+        pathname: '/(recovery)/changePassword',
+        params: { token: token },
+      });
+    }
+    if (data.status === 200 && data.msg === 'Token no valido') {
+      setInvalidToken(true);
+    }
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    if (error) {
+      if (error.status === 498) return router.replace('/(recovery)/olvidasteContrasenia');
+      if (error.status === 500) return router.replace('/oops');
+    }
+  }, [error]);
+
+  const handlePress = async () => {
+    setTrigger(true);
+  };
 
   return (
     <KeyboardAvoidingView behavior='padding' className="flex-1 bg-background">
@@ -28,7 +59,12 @@ const InsertToken = () => {
             handleChangeText={setToken} 
           />
         </View>
-        <View className='my-9' style={{ width: 300 }}>
+        <View className='my-9 gap-3' style={{ width: 300 }}>
+          {invalidToken && (
+            <Text className='text-red-500 text-center'>
+              El token no es valido
+            </Text>
+          )}
           <ConfirmButton title='Validar token' onPress={handlePress} disabled={(token == '')}></ConfirmButton>
         </View>
       </View>
